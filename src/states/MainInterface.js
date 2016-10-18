05394bcf91fd0cacf3820d420537fa22b7d08801
Robbie Antenesse <gamedev@alamantus.com>
2016-10-17 23:26:49 -0600

    Started to try some math, but need to test.

--- src/states/MainInterface.js
+++ src/states/MainInterface.js
@@ -34,6 +34,25 @@ export class MainInterface extends Phaser.State {
     this.compass.anchor.x = 0.5;
     this.compass.anchor.y = 0.5;
     this.compass.nav = new Nav(this.game);
+
+    this.item = this.add.sprite(this.game.width / 2, this.game.height / 4, 'red-square');
+    this.item.anchor.x = 0.5;
+    this.item.anchor.y = 0.5;
+    this.item.latitude = this.compass.nav.latitude + 0.01;
+    this.item.longitude = this.compass.nav.longitude + 0.008;
+    let forwardVector = {x: 0, y: 1};
+    let forwardVectorNormalized = 1;
+    let itemVector = {
+      x: Math.abs(this.compass.nav.latitude - this.item.latitude),
+      y: Math.abs(this.compass.nav.longitude - this.item.longitude)
+    }
+    let itemVectorNormalized = Math.sqrt((itemVector.x * itemVector.x) + (itemVector.y * itemVector.y));
+    let dotProduct = (forwardVector.x * itemVector.x) + (forwardVector.y + itemVector.y);
+    let relativeAngle = Math.acos((dotProduct / (forwardVectorNormalized * itemVectorNormalized)));
+    // Pretty sure the acos of relativeAngle and the cos below cancel out, but we'll see.
+    // Also, itemVectorNormalized should be the length of the line from the center to the item, which would be radius.
+    this.item.x = this.compass.x + (itemVectorNormalized * Math.cos(relativeAngle));
+    this.item.y = this.compass.y + (itemVectorNormalized * Math.cos(relativeAngle));
   }
 
   update () {
