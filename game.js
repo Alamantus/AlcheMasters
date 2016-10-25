159c91885aa54e37d3e80a1b2d8208b8466ed8ea
Robbie Antenesse <gamedev@alamantus.com>
2016-10-25 17:03:09 -0600

    Playing with pickup item scale by distance!

--- game.js
+++ game.js
@@ -755,16 +755,16 @@
 	      this.player.nav.update();
 	      if (this.player.nav.type == 'prod') {}
 	
-	      /*this.itemCheckFrameDelay--;
+	      this.itemCheckFrameDelay--;
 	      if (this.hasGeneratedItems) {
 	        if (this.itemCheckFrameDelay <= 0) {
-	          this.map.pickups.forEach((pickup) => {
-	            pickup.pickup.updatePosition();
+	          this.map.pickups.forEach(function (pickup) {
+	            pickup.pickup.updateScaleByDistance();
 	          });
 	          // Only check items once every this number of frames.
 	          this.itemCheckFrameDelay = window.settings.itemCheckDelayNumberOfFrames;
 	        }
-	      }*/
+	      }
 	
 	      this.worldgroup.pivot.x = this.player.x;
 	      this.worldgroup.pivot.y = this.player.y;
@@ -811,7 +811,7 @@
 	
 	      this.map.pickups.forEach(function (pickup) {
 	        pickup.anchor.setTo(0.5, 0.5);
-	        // pickup.pickup = new Pickup(pickup, this.player, 60, 180);
+	        pickup.pickup = new _Pickup.Pickup(pickup, _this3.player, 60, 180);
 	        // console.log(pickup.pickup.life);
 	
 	        _this3.worldgroup.add(pickup);
@@ -1688,7 +1688,7 @@
 	'use strict';
 	
 	Object.defineProperty(exports, "__esModule", {
-	    value: true
+	  value: true
 	});
 	exports.MapSpriteController = undefined;
 	
@@ -1699,95 +1699,100 @@
 	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 	
 	var MapSpriteController = exports.MapSpriteController = function () {
-	    function MapSpriteController(parentObject, compassObject) {
-	        _classCallCheck(this, MapSpriteController);
+	  function MapSpriteController(parentObject, compassObject) {
+	    _classCallCheck(this, MapSpriteController);
 	
-	        this.parent = parentObject;
-	        this.parent.anchor.x = 0.5;
-	        this.parent.anchor.y = 0.5;
+	    this.parent = parentObject;
+	    this.parent.anchor.x = 0.5;
+	    this.parent.anchor.y = 0.5;
 	
-	        this.compass = compassObject;
+	    this.state = parentObject.game.state.getCurrentState();
 	
-	        // I belive this will only be used for testing! LatLong will likely be distributed based on positions
-	        // at regular 0.005 (or something like that) global latlong intervals that the player is closest to.
-	        var LATLONGMAXDISTANCE = 0.002;
-	        this.longitude = this.compass.nav.longitude + (0, _helpers.getRandom)(-LATLONGMAXDISTANCE, LATLONGMAXDISTANCE);
-	        this.latitude = this.compass.nav.latitude + (0, _helpers.getRandom)(-LATLONGMAXDISTANCE, LATLONGMAXDISTANCE);
-	        // console.log('item latlong: ' + this.longitude + ', ' + this.latitude);
+	    this.compass = compassObject;
 	
-	        this.updatePosition();
+	    // I belive this will only be used for testing! LatLong will likely be distributed based on positions
+	    // at regular 0.005 (or something like that) global latlong intervals that the player is closest to.
+	    // const LATLONGMAXDISTANCE = 0.002;
+	    // this.longitude = this.compass.nav.longitude + getRandom(-LATLONGMAXDISTANCE, LATLONGMAXDISTANCE);
+	    // this.latitude = this.compass.nav.latitude + getRandom(-LATLONGMAXDISTANCE, LATLONGMAXDISTANCE);
+	    // console.log('item latlong: ' + this.longitude + ', ' + this.latitude);
+	
+	    // this.updatePosition();
+	  }
+	
+	  _createClass(MapSpriteController, [{
+	    key: 'updateScale',
+	    value: function updateScale() {
+	      var distance = this.state.math.distance(this.compass.x, this.compass.y, this.parent.x, this.parent.y),
+	          halfSprite = this.compass.width / 2;
+	
+	      if (distance > halfSprite) {
+	        var targetScale = halfSprite / distance;
+	        this.parent.scale = this.state.math.linear(this.parent.scale, this.targetScale, window.settings.lerpPercent);
+	      } else {
+	        if (this.parent.scale !== 1) {
+	          this.parent.scale = 1;
+	        }
+	      }
 	    }
 	
-	    _createClass(MapSpriteController, [{
-	        key: 'calcPosition',
-	        value: function calcPosition(pixelScale) {
-	            // console.log('pixelScale = ' + pixelScale);
-	            var LATLONGTOPIXELADJUSTMENT = 1000;
-	            // console.log('LATLONGTOPIXELADJUSTMENT = ' + LATLONGTOPIXELADJUSTMENT);
-	
-	            var itemOffset = {
-	                x: (this.compass.nav.longitude - this.longitude) * LATLONGTOPIXELADJUSTMENT,
-	                y: (this.compass.nav.latitude - this.latitude) * LATLONGTOPIXELADJUSTMENT
-	            };
-	            // console.log('item geoposition offset = ' + (itemOffset.x / LATLONGTOPIXELADJUSTMENT) + ', ' + (itemOffset.y / LATLONGTOPIXELADJUSTMENT));
-	
-	            // radius should be the length of the line from the center to the item.
-	            var radius = Math.sqrt(itemOffset.x * itemOffset.x + itemOffset.y * itemOffset.y);
-	            // console.log('radius = ' + radius);
-	
-	            // Calculate the distance between forward point and item position.
-	            var distanceBetweenPoints = Math.sqrt((0, _helpers.square)(0 - itemOffset.x) + (0, _helpers.square)(radius - itemOffset.y));
-	            // console.log('distanceBetweenPoints = ' + distanceBetweenPoints);
-	
-	            var doubleRadiusSquared = 2 * (0, _helpers.square)(radius);
-	            // console.log('doubleRadiusSquared = ' + doubleRadiusSquared);
-	
-	            var insideArcCos = (doubleRadiusSquared - (0, _helpers.square)(distanceBetweenPoints)) / doubleRadiusSquared;
-	            // console.log('insideArcCos = ' + insideArcCos);
-	
-	            if (this.compass.nav.headingHasChanged) {
-	                this.displayAngle = Math.acos(insideArcCos) - (0, _helpers.radians)(this.compass.nav.heading - 90);
-	            }
-	            // console.log('this.displayAngle = ' + this.displayAngle);
-	
-	            // The xAdjustmentvalues equate to the itemOffset and radius values so we can use inverseLerp.
-	            var minAdjustmentValue = window.settings.minPixelDistance / pixelScale;
-	            var maxAdjustmentValue = window.settings.maxPixelDistance / pixelScale;
-	
-	            if (this.compass.nav.positionHasChanged) {
-	                // This returns a distance scaled by a scaled pixelScale. The closer the object is, the lower the pixelScale, and the farther something is, the larger the pixelScale.
-	                // This makes the object display farther away when it's farther away but approach quickly as you get closer by reducing the radius scale.
-	                // The pixelDistance is then controlled by the max and min pixelDistance settings.
-	                this.pixelDistance = radius * (pixelScale * (0, _helpers.inverseLerp)(minAdjustmentValue, maxAdjustmentValue, radius));
-	                if (this.pixelDistance > window.settings.maxPixelDistance) {
-	                    this.pixelDistance = window.settings.maxPixelDistance;
-	                }
-	                // if (this.pixelDistance < window.settings.minPixelDistance) {
-	                //   this.pixelDistance = window.settings.minPixelDistance;
-	                // }
-	            }
-	            // console.log('pixelDistance = ' + this.pixelDistance);
-	
-	            var result = {
-	                x: Math.round(this.compass.x + this.pixelDistance * Math.cos(this.displayAngle)),
-	                y: Math.round(this.compass.y + this.pixelDistance * Math.sin(this.displayAngle))
-	            };
-	            // console.log('item at: ' + result.x + ', ' + result.y);
-	
-	            return result;
+	    /*calcPosition (pixelScale) {
+	      // console.log('pixelScale = ' + pixelScale);
+	      const LATLONGTOPIXELADJUSTMENT = 1000;
+	      // console.log('LATLONGTOPIXELADJUSTMENT = ' + LATLONGTOPIXELADJUSTMENT);
+	        let itemOffset = {
+	        x: (this.compass.nav.longitude - this.longitude) * LATLONGTOPIXELADJUSTMENT
+	      , y: (this.compass.nav.latitude - this.latitude) * LATLONGTOPIXELADJUSTMENT
+	      }
+	      // console.log('item geoposition offset = ' + (itemOffset.x / LATLONGTOPIXELADJUSTMENT) + ', ' + (itemOffset.y / LATLONGTOPIXELADJUSTMENT));
+	        // radius should be the length of the line from the center to the item.
+	      let radius = Math.sqrt((itemOffset.x * itemOffset.x) + (itemOffset.y * itemOffset.y));
+	      // console.log('radius = ' + radius);
+	        // Calculate the distance between forward point and item position.
+	      let distanceBetweenPoints = Math.sqrt(square(0 - (itemOffset.x)) + square(radius - (itemOffset.y)))
+	      // console.log('distanceBetweenPoints = ' + distanceBetweenPoints);
+	        let doubleRadiusSquared = 2 * square(radius);
+	      // console.log('doubleRadiusSquared = ' + doubleRadiusSquared);
+	        let insideArcCos = (doubleRadiusSquared - square(distanceBetweenPoints)) / doubleRadiusSquared;
+	      // console.log('insideArcCos = ' + insideArcCos);
+	        if (this.compass.nav.headingHasChanged) {
+	        this.displayAngle = Math.acos(insideArcCos) - radians(this.compass.nav.heading - 90);
+	      }
+	      // console.log('this.displayAngle = ' + this.displayAngle);
+	        // The xAdjustmentvalues equate to the itemOffset and radius values so we can use inverseLerp.
+	      let minAdjustmentValue = window.settings.minPixelDistance / pixelScale;
+	      let maxAdjustmentValue = window.settings.maxPixelDistance / pixelScale;
+	        if (this.compass.nav.positionHasChanged) {
+	        // This returns a distance scaled by a scaled pixelScale. The closer the object is, the lower the pixelScale, and the farther something is, the larger the pixelScale.
+	        // This makes the object display farther away when it's farther away but approach quickly as you get closer by reducing the radius scale.
+	        // The pixelDistance is then controlled by the max and min pixelDistance settings.
+	        this.pixelDistance = radius * (pixelScale * inverseLerp(minAdjustmentValue, maxAdjustmentValue, radius));
+	        if (this.pixelDistance > window.settings.maxPixelDistance) {
+	          this.pixelDistance = window.settings.maxPixelDistance;
 	        }
-	    }, {
-	        key: 'updatePosition',
-	        value: function updatePosition() {
-	            if (this.compass.nav.hasChanged) {
-	                var positionOnScreen = this.calcPosition(window.settings.pixelScale);
-	                this.parent.x = (0, _helpers.lerp)(this.parent.x, positionOnScreen.x, window.settings.lerpPercent);
-	                this.parent.y = (0, _helpers.lerp)(this.parent.y, positionOnScreen.y, window.settings.lerpPercent);
-	            }
-	        }
-	    }]);
+	        // if (this.pixelDistance < window.settings.minPixelDistance) {
+	        //   this.pixelDistance = window.settings.minPixelDistance;
+	        // }
+	      }
+	      // console.log('pixelDistance = ' + this.pixelDistance);
+	        let result = {
+	        x: Math.round(this.compass.x + (this.pixelDistance * Math.cos(this.displayAngle)))
+	      , y: Math.round(this.compass.y + (this.pixelDistance * Math.sin(this.displayAngle)))
+	      }
+	      // console.log('item at: ' + result.x + ', ' + result.y);
+	        return result;
+	    }
+	      updatePosition () {
+	      if (this.compass.nav.hasChanged) {
+	        let positionOnScreen = this.calcPosition(window.settings.pixelScale);
+	        this.parent.x = lerp(this.parent.x, positionOnScreen.x, window.settings.lerpPercent);
+	        this.parent.y = lerp(this.parent.y, positionOnScreen.y, window.settings.lerpPercent);
+	      }
+	    }*/
 
-	    return MapSpriteController;
+	  }]);
+
+	  return MapSpriteController;
 	}();
 
 /***/ },
